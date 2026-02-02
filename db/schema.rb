dd153ebde526d7317dbf67ec46729815dc5d450b
yukki091205-ui <yukki091205@gmail.com>
2026-02-02 20:41:55 +0900

    ログイン必須化と予約のユーザー紐付けを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_01_080305) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_02_091305) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_01_080305) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "salon_name"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_01_080305) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "reservations", "users"
 end
