452812a01a890ae98a43436f2d32c6349b6233bb
yukki091205-ui <yukki091205@gmail.com>
2026-02-05 02:24:02 +0900

    CI向けにsystem testスキップ処理の書式を修正

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,10 +1,10 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  if ENV['CI']
-  puts 'CI環境のため system test をスキップします'
+  if ENV["CI"]
+  puts "CI環境のため system test をスキップします"
   exit 0
-end
+  end
 
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
 end
