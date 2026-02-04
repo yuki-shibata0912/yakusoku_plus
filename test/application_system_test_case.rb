4d5e42cb12b03fd1d9f7e3c9795dd14264c3bb55
yukki091205-ui <yukki091205@gmail.com>
2026-02-05 02:40:05 +0900

    CI実行時のsystem testを無効化

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,9 +2,12 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   if ENV["CI"]
-  puts "CI環境のため system test をスキップします"
-  exit 0
+    # CIでは system test を全部スキップ（失敗にしない）
+    def before_setup
+      super
+      skip "Skip system tests on CI"
+    end
+  else
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
   end
-
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
 end
