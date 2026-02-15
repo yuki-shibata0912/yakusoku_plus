476588702826717dfc30c5cf95211fff0d74de81
yukki091205-ui <yukki091205@gmail.com>
2026-02-15 20:59:03 +0900

    テストでDeviseのログインを有効化

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,16 +4,8 @@ require "rails/test_help"
 
 require "devise"
 
-module ActiveSupport
-  class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
-
-    # Add more helper methods to be used by all tests here...
-  end
+class ActiveSupport::TestCase
+  fixtures :all
 end
 
 class ActionDispatch::IntegrationTest
